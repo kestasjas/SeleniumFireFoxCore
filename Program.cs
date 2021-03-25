6445fc32f7509836e32faf50f9f4c038d3bb0b94
Kestas Jasinevicius <kestasj@comcast.net>
2021-03-25 13:57:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumFireFoxCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver ffox = new FirefoxDriver(
+                           Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
+                           new FirefoxOptions());
+
+            ffox.Manage().Window.Maximize();
+            ffox.Navigate().GoToUrl("https://www.google.com/");
+            ffox.Quit();
+        }
+    }
+}
